beacc68efe8357a458e4b136def73533df686d70
saturneric <eric@bktus.com>
2026-04-11 17:53:09 +0200

    feat(rust): add automated C++ header generation
    * Introduce a build script and configuration to automatically generate
    C++ bindings using `cbindgen`
    * Ensure the generated header is placed in the core source directory
    to facilitate seamless integration between Rust and C++ components
    * Configure the generator to use the `GpgFrontend::Rust` namespace and
    include appropriate header guards

---
+++ rust/build.rs
@@ -0,0 +1,30 @@
+extern crate cbindgen;
+
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let mut output_file = PathBuf::from(&crate_dir);
+    output_file.pop();
+    output_file.push("src");
+    output_file.push("core");
+
+    if !output_file.exists() {
+        fs::create_dir_all(&output_file).expect("failed to create include directory");
+    }
+
+    output_file.push("GpgCoreRust.h");
+
+    let config = cbindgen::Config::from_root_or_default(&crate_dir);
+
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_config(config)
+        .with_language(cbindgen::Language::Cxx)
+        .generate()
+        .expect("failed to generate binding file")
+        .write_to_file(output_file);
+}
